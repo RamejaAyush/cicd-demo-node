b4846c12ffbe497835b058828782d90fbc67d77f
RamejaAyush <ayushrwork@gmail.com>
2023-06-22 22:32:41 +0530

    Added Template Server code

---
+++ server.js
@@ -0,0 +1,12 @@
+const PORT = 8080
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.status(200).send('Welcome to the demo CI/CD Application')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
