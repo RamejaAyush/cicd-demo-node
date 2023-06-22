32c1af3e5d295d4b30d95e58208ecb11e0187481
RamejaAyush <ayushrwork@gmail.com>
2023-06-22 22:46:59 +0530

    Added one basic route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express()
 app.get('/', (req, res) => {
   return res.status(200).send('Welcome to the demo CI/CD Application')
 })
+app.get('/sad', (req, res) => {
+  return res.status(200).send('Im Sad')
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
